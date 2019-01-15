e5160e2cecfcc21206002769521b8974e2e014f8
Patrick Baxter <Patrickbaxtercom@gmail.com>
2019-01-15 21:35:42 +1300

    npm initialize

--- source/drawpick.js
+++ source/drawpick.js
@@ -2,10 +2,14 @@ let canvas = document.querySelector('#canvas')
 let generationCount = document.querySelector('#generationCount')
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const centerX = canvas.width / 2
-const centerY = canvas.height / 2
+
 const length = 30
 
+const resizeCanvas = () =>{
+    console.log('test')
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
 
 const addGeneration = () => {
     generationCount.innerHTML++
@@ -26,13 +30,37 @@ const resetGeneration = () => {
 
 const confirm = () => {
     generation = Number(generationCount.innerHTML);
-    if (canvas.getContext) {
-        let context = canvas.getContext('2d');
-        for(let x = 0; x <= generation; x++){
-            context.moveTo(centerX,centerY)
-            context.lineTo(centerX + length/2, centerY)
-            context.lineTo(centerX - length/2, centerY)
-            context.stroke();
-        }
+    drawLine(generation)
+}
+
+function line(x,y){
+    this.x = x
+    this.y = y
+}
+
+const drawLine = (genCount, ratio) =>{
+    const centerX = canvas.width / 2
+    const centerY = canvas.height / 2
+    let length = 50
+    let totalLines = 2
+    let lines = [new line(centerX - length/2,centerY), new line(centerX + length/2 ,centerY)]
+    let context = canvas.getContext('2d');
+
+    context.moveTo(lines[0].x,lines[0].y)
+    context.lineTo(lines[1].x, lines[1].y)
+    context.stroke();
+
+    for (let x = 0; x < genCount; x++) {
+        length *= ratio
+        for(let y = 0; y < totalLines; y++){
+            let l = lines.pop()
+            if(x % 2 === 0){
+                
+            } else {
+                context.moveTo(l.x, l.y - length/2)
+                context.lineTo(l.x. l.y + length/2)
+                context.stroke()
+            }
+        }      
     }
 }
